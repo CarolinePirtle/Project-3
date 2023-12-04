833c081e68ea2b44bd4943e0123127655df6c7e7
CarolinePirtle <cpirtle919@gmail.com>
2023-12-04 16:27:31 -0500

    Setting up inital UI

--- gps-app/src/app/app.component.ts
+++ gps-app/src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { GpsInterfaceComponent } from './gps-interface/gps-interface.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet, GpsInterfaceComponent],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/favicon.ico" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-gps-interface></app-gps-interface>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
