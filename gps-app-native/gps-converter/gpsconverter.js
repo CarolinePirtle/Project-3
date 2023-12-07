8cb95b6ed8cce16597bc933d825383b65ebaa2d9
CarolinePirtle <cpirtle919@gmail.com>
2023-12-07 16:54:49 -0500

    Updated input

--- gps-app-native/gps-converter/gpsconverter.js
+++ gps-app-native/gps-converter/gpsconverter.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 const leaps = [46828800, 78364801, 109900802, 173059203, 252028804, 315187205, 346723206, 393984007, 425520008, 457056009, 504489610, 
                 551750411, 599184012, 820108813, 914803214, 1025136015, 1119744016, 1167264017]
 
@@ -148,8 +148,8 @@ export default function ConverterFunc() {
        onChangeText={newText=>setText(newText)}
      />
       <Button title='Convert' 
-          onPress={() => UTC2gps(2003,12,2)} />
-          <Text>Result: {result}</Text>
+          onPress={() => UTC2gps(text)} />
+          <Text>Result: {gps_time}</Text>
      </View>
 
    );
